dc3f39a23d6aa024f97ae603c7d8955e7c19f345
barack0b4m4 <adanahesa@gmail.com>
2022-03-12 14:18:22 -0500

    removed old item shit

--- deathmatch/resources/items/items_s.lua
+++ deathmatch/resources/items/items_s.lua
@@ -42,24 +42,6 @@ addEventHandler('itemcreator:pagetwo', root, itemcreatorTypes)
 
 addCommandHandler('itemcreator', launchItemCreator)
 
-local function giveItem(source, commandName, item_id)
-    
-    local player = source
-        if getElementData(player, 'invslot:1') == false or getElementData(player, 'invslot:1') == 0
-            then setElementData(player, 'invslot:1', item_id)
-            outputChatBox('You have received the item', player, 100, 100, 100)
-        elseif getElementData(player, 'invslot:2') == false or getElementData(player, 'invslot:2') == 0
-            then setElementData(player, 'invslot:2', item_id)
-            outputChatBox('You have received the item', player, 100, 100, 100)
-        elseif getElementData(player, 'invslot:3') == false or getElementData(player, 'invslot:3') == 0
-            then setElementData(player, 'invslot:3', item_id)
-                outputChatBox('You have received the item', player, 100, 100, 100)
-        else
-        return outputChatBox('The player has no space for this item', player, 255, 100, 100)
-        end
-    
-end
-
 local function checkInventory(source, commandName, slot_number)
     local slot = getElementData(source, slot_number)
     iprint(slot)
@@ -71,37 +53,6 @@ local function checkInventory(source, commandName, slot_number)
             outputChatBox('This slot has a '.. results[1].item_name ..' in it', source, 255, 255, 255)
         end
 end
-
-
-local function useItemg(source, commandName, slot_number)
-    local slot = getElementData(source, slot_number)
-    local food = tostring(food)
-    local results = dbPoll ( dbQuery  (db, "SELECT item_type FROM items WHERE item_id =?", slot), -1 )
-    local currentHunger = getElementData(source, 'hunger')
-    iprint(results)
-        if slot == false
-        then 
-            return outputChatBox('There is nothing in this slot', source, 255, 100, 100)
-
-        elseif results[1].item_type == 'food' 
-
-        then 
-            if currentHunger >= 0 then
-            local effects = dbPoll ( dbQuery  (db, "SELECT attribute_1 FROM items WHERE item_id =?", slot), -1 )
-            setElementData(source, slot_number, 0)
-            setElementData(source,'hunger', currentHunger - effects[1].attribute_1)
-            outputChatBox('You ate a food item', source, 100, 255, 100)
-            else 
-               return outputChatBox('You arent hungry', source, 255, 100, 100)
-            end
-        else
-
-        end
-    end
-
-addCommandHandler('giveitem', giveItem)
-
-
 
 local function checkInv(source)
     local inv = getElementData(source, 'inventory')
